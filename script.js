05557f1fa6909e28edc14c785cd932dc1e16610c
DorianekBSP <112347529+DorianekBSP@users.noreply.github.com>
2023-06-05 18:15:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ const photoElement = document.getElementById('photo');
 const totalPhotos = 41;
 let currentPhoto = 1;
 
-document.getElementById("left").style.height = photoElement.offsetHeight + "px";
-document.getElementById("right").style.height = photoElement.offsetHeight + "px";
-
 function previousPhoto() {
   if (currentPhoto > 1) {
     currentPhoto--;
