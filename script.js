e5e2149caade4ef83d6708458f5d44c13543811f
DorianekBSP <112347529+DorianekBSP@users.noreply.github.com>
2023-06-10 19:45:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,19 +8,41 @@ function previousPhoto() {
   if (currentPhoto > 1) {
     currentPhoto--;
   }
-  photoElement.src = "Photos/Photo (" + currentPhoto + ").JPG";
+  photoElement.src = `Photos/Photo (${currentPhoto}).JPG`;
+  updatePhotoNumber();
 }
+
 function nextPhoto() {
   if (currentPhoto < totalPhotos) {
-    currentPhoto ++;
+    currentPhoto++;
   }
-  photoElement.src = "Photos/Photo (" + currentPhoto + ").JPG";
+  photoElement.src = `Photos/Photo (${currentPhoto}).JPG`;
+  updatePhotoNumber();
 }
+
+function jumpToPhoto() {
+  const photoNumberInput = document.getElementById("photoNumberInput");
+  const photoNumber = parseInt(photoNumberInput.value);
+  if (photoNumber >= 1 && photoNumber <= totalPhotos) {
+    currentPhoto = photoNumber;
+    photoElement.src = `Photos/Photo (${currentPhoto}).JPG`;
+    updatePhotoNumber();
+  }
+}
+
+function updatePhotoNumber() {
+  const currentPhotoNumber = document.getElementById("currentPhotoNumber");
+  currentPhotoNumber.textContent = currentPhoto.toString();
+}
+
 function setButtonHeight() {
   const size = photoElement.offsetHeight + "px"
-  right.height = size
-  left.height = size
+  right.style.height = size;
+  left.style.height = size;
 }
 
 window.addEventListener('resize', setButtonHeight);
-window.addEventListener('DOMContentLoaded', setButtonHeight);
+window.addEventListener('DOMContentLoaded', function() {
+  setButtonHeight();
+  updatePhotoNumber();
+});
