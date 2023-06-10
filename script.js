a64fb1c3f5a8258b3e5a9025165e6f52731701a3
DorianekBSP <112347529+DorianekBSP@users.noreply.github.com>
2023-06-10 20:11:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
-const currentPhotoNumber = document.getElementById("currentPhotoNumber");
-const photoNumberInput = document.getElementById("photoNumberInput");
-const photoElement = document.getElementById('photo');
-const right = document.getElementById("right");
-const left = document.getElementById("left");
-const totalPhotos = 41;
-let currentPhoto = 1;
+window.addEventListener('load', function() {
+  const currentPhotoNumber = document.getElementById("currentPhotoNumber");
+  const photoNumberInput = document.getElementById("photoNumberInput");
+  const photoElement = document.getElementById('photo');
+  const right = document.getElementById("right");
+  const left = document.getElementById("left");
+  const totalPhotos = 41;
+  let currentPhoto = 1;
 
-function previousPhoto() {
-  if (currentPhoto > 1) {
-    currentPhoto--;
-  }
-  photoElement.src = `Photos/Photo (${currentPhoto}).JPG`;
-  currentPhotoNumber.textContent = currentPhoto.toString();
+  function previousPhoto() {
+    if (currentPhoto > 1) {
+      currentPhoto--;
+    }
+    photoElement.src = `Photos/Photo (${currentPhoto}).JPG`;
+    currentPhotoNumber.textContent = currentPhoto.toString();
 }
 
-function nextPhoto() {
-  if (currentPhoto < totalPhotos) {
-    currentPhoto++;
-  }
-  photoElement.src = `Photos/Photo (${currentPhoto}).JPG`;
-  currentPhotoNumber.textContent = currentPhoto.toString();
-}
-
-function jumpToPhoto() {
-  const photoNumber = parseInt(photoNumberInput.value);
-  if (photoNumber >= 1 && photoNumber <= totalPhotos) {
-    currentPhoto = photoNumber;
+  function nextPhoto() {
+    if (currentPhoto < totalPhotos) {
+      currentPhoto++;
+    }
     photoElement.src = `Photos/Photo (${currentPhoto}).JPG`;
     currentPhotoNumber.textContent = currentPhoto.toString();
   }
-}
 
-function setButtonHeight() {
-  const size = photoElement.offsetHeight + "px"
-  right.style.height = size;
-  left.style.height = size;
-}
+  function jumpToPhoto() {
+    const photoNumber = parseInt(photoNumberInput.value);
+    if (photoNumber >= 1 && photoNumber <= totalPhotos) {
+      currentPhoto = photoNumber;
+      photoElement.src = `Photos/Photo (${currentPhoto}).JPG`;
+      currentPhotoNumber.textContent = currentPhoto.toString();
+    }
+  }
+  
+  function setButtonHeight() {
+    const size = photoElement.offsetHeight + "px"
+    right.style.height = size;
+    left.style.height = size;
+  }
+});
