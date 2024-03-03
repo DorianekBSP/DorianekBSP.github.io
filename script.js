4a6cff76fd11dc31d88750c436c33c3d534f57fe
NamelessPL <112347529+DorianekBSP@users.noreply.github.com>
2024-03-03 14:52:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const totalPhotos = 81;
+const totalPhotos = 85;
 let currentPhoto = 1;
+let shouldSkip = false;
 let isEnglish = false;
 
 function previousPhoto() {
@@ -30,16 +31,6 @@ function jumpToPhoto() {
 	}
 }
 
-function loadButtonsSize() {
-	let previousSize = document.getElementById("photo").offsetHeight
-	document.getElementById("left").style.height = document.getElementById("photo").offsetHeight + "px";
-	document.getElementById("right").style.height = document.getElementById("photo").offsetHeight + "px";
-	if (previousSize == document.getElementById("photo").offsetHeight) {
-		setTimeout(loadButtonsSize);
-	}
-}
-window.addEventListener('resize', loadButtonsSize);
-
 function toggleLanguage() {
 	isEnglish = !isEnglish;
 	if (isEnglish) {
@@ -55,34 +46,31 @@ function toggleLanguage() {
 	}
 }
 
-let shouldSkip = false;
+function loadButtonsSize() {
+	let previousSize = document.getElementById("photo").offsetHeight
+	document.getElementById("left").style.height = document.getElementById("photo").offsetHeight + "px";
+	document.getElementById("right").style.height = document.getElementById("photo").offsetHeight + "px";
+	if (previousSize == document.getElementById("photo").offsetHeight) {
+		setTimeout(loadButtonsSize);
+	}
+}
+window.addEventListener('resize', loadButtonsSize);
 function skipLoading() {
 	shouldSkip = true;
 	loadButtonsSize();
 	document.getElementById("loadingScreen").style.display = "none";
 	document.getElementById("displayItAfterLoading").style.display = "block";
 }
-
-let preloadedImages = [];
-function preloadImages() {
-	for (let i = 1; i <= totalPhotos; i++) {
-		let img = new Image();
-		img.src = "Photos/" + "Photo (" + [i] + ").JPG";
-        preloadedImages.push(img);
-		img.onload = function() {
-			document.getElementById("loadingLabel").innerHTML = "Ładowanie/Loading... (" + i + "/" + totalPhotos + ")";
-			if (i == totalPhotos) {
-				document.getElementById("loadingScreen").style.display = "none";
-				document.getElementById("displayItAfterLoading").style.display = "block";
-			}
-		}
-		if (shouldSkip) {
-			break;
+function preloadImages(i) {
+	let img = new Image();
+	img.src = "Photos/" + "Photo (" + [i] + ").JPG";
+	img.onload = function() {
+		document.getElementById("loadingLabel").innerHTML = "Ładowanie/Loading... (" + i + "/" + totalPhotos + ")";
+		if (shouldSkip == false && i != totalPhotos) {
+			preloadImages(i + 1);
+		} else {
+			skipLoading();
 		}
 	}
 }
-window.onload = preloadImages;
-document.addEventListener("DOMContentLoaded", function () {
-  preloadImages();
-});
-preloadImages();
+preloadImages(1);
